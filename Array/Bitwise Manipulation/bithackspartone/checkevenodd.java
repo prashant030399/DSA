4cf96e0585c25187d902a4e31befd3a227e3a016
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-09-17 12:14:31 +0530

    Add files via upload

---
+++ Array/Bitwise Manipulation/bithackspartone/checkevenodd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class  checkevenodd{
+
+    public static void main(String args []){
+
+Scanner sc = new Scanner (System.in);
+int n = sc.nextInt();
+
+if((n & 1)!=0){
+    System.out.print(n+ " is odd ");
+}
+else {
+    System.out.print(n + " is even");
+}
+
+
+    }
+
+}
