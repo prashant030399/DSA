4c3f56d00d5e6b3f8d4c78ecf9b59e2f1ba9d017
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-09-23 12:05:03 +0530

    Add files via upload

---
+++ Array/Stack/pushbasic.java
@@ -0,0 +1,44 @@
+class Stack {
+
+    int [] x = new int [5];
+    int top = -1;
+
+    void push (int val ){
+        if(top>=4){
+            System.out.print("overflow" + " ");
+        }
+        else {
+            top ++;
+            x[top] = val;
+            System.out.print("pushed" + " ");
+        }
+    }
+
+
+    void print(){
+        for(int i=top; i>=0 ; i--){
+            System.out.print(x[i] + " ");
+        }
+    }
+
+
+    
+public static void main(String args []){
+
+Stack s = new Stack();
+s.push(10);
+s.push(20);
+s.push(30);
+s.push(40);
+s.push(50);
+s.push(60);
+s.push(70);
+s.print();
+    
+}
+
+}
+           
+
+
+
