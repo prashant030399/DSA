0e53a68091352317a43b138552ad6ac169b806a2
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-09-30 12:03:11 +0530

    Add files via upload

---
+++ Stack/Strings/codeforwin Strings/problem7.java
@@ -0,0 +1,24 @@
+public class problem7 {
+
+ public static void main(String[] args) {
+        
+        String x = "Prashant";
+String y="";
+        for(int i=0; i<x.length(); i++){
+
+            char p = x.charAt(i);
+if(p>=65 && p<=90){
+    y = y + ((char)(p+32));
+}
+else if(p>=97 && p<=122){
+    y = y + ((char)(p-32));
+}
+        }
+System.out.print(y);
+        
+
+
+
+    }
+    
+}
