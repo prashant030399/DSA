42cb65fcf54af69c5f9fcf872b9f212afad189b1
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-01 12:23:29 +0530

    Add files via upload

---
+++ Stack/Strings/codeforwin Strings/problem13.java
@@ -0,0 +1,24 @@
+//Write a Java Program to reverse order of a word
+
+import java.util.*;
+
+public class problem13 {
+    public static void main(String args []){
+Scanner sc = new Scanner (System.in);
+String x = sc.nextLine();
+
+//split would convert into an array
+String y [] = x.split(" ");
+
+for(int i=y.length -1 ; i>=0 ;i--){
+  System.out.print(y[i] + " ");
+}
+
+
+
+
+
+    }
+
+    
+}
