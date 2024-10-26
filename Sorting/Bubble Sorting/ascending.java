433dad7d7c5a32ab76e2759d8e3db7d42fcfca7f
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-26 12:05:36 +0530

    Add files via upload

---
+++ Sorting/Bubble Sorting/ascending.java
@@ -0,0 +1,22 @@
+public class ascending {
+
+    public static void main(String[] args) {
+        
+        int a[] = {4,2,8,9,3};
+        for(int i=0 ; i<a.length ; i++){
+            for(int j=0 ; j<a.length-1-i ; j++){
+                if(a[j]>a[j+1]){
+                    a[j] = a[j] + a[j+1];
+                    a[j+1] = a[j] - a[j+1];
+                    a[j] = a[j] - a[j+1];
+                }
+            }
+        }
+
+for(int i=0 ; i<a.length ; i++){
+    System.out.print(a[i] + " ");
+}
+
+    }
+    
+}
