b7413135bdf99fee866cc37998f9baf01724f0ed
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-30 20:27:11 +0530

    Add files via upload

---
+++ Bitwise Manipulation/bithackspartone/addoneinteger.java
@@ -0,0 +1,15 @@
+
+
+public class addoneinteger {
+public static void main(String args []){
+
+
+    int a = 4;
+    System.out.println(a);
+    System.out.println(~a);
+    System.out.println(-~a);
+ 
+
+}
+    
+}
