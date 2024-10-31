431941a41e977fb2c63d3f0b4bf2cbde979c1de3
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-31 12:05:53 +0530

    Add files via upload

---
+++ Bitwise Manipulation/bithackspartone/problem2.java
@@ -0,0 +1,16 @@
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        int x = 4;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+ 
+        x = -5;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+ 
+        x = 0;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+    }
+
+    
+}
