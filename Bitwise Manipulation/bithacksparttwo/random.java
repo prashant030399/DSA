93c59ca8510d8786cf8af63df15779f9ed8a95cb
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-11-01 12:06:36 +0530

    Add files via upload

---
+++ Bitwise Manipulation/bithacksparttwo/random.java
@@ -0,0 +1,24 @@
+public class random {
+
+    public static void  main (String args []){
+
+int  n=6; int f=1; 
+for(int i=1 ; i<=n ; i++){
+
+    // for(int j=n ; j>=1 ;j-- ){
+    //     f= f * j;
+    // }
+    33
+    f=f*i;
+
+    if(f<n){
+        System.out.print(f + " ");
+    }else{
+        break;
+    }
+
+}
+
+    } 
+    
+}
