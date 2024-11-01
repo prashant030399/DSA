52f6c097607d74a9ce4da1663a5851aa1d260418
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-11-01 12:04:34 +0530

    Add files via upload

---
+++ Bitwise Manipulation/bithacksparttwo/minimumbitreq.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class minimumbitreq {
+
+    public static void main(String args []){
+Scanner sc = new Scanner (System.in);
+int n = sc.nextInt();
+
+for(int i=31; i>=0 ; i--){
+    int s=1 <<i;
+
+    if( (n &s )!=0  ){
+        System.out.print(i+1);
+        break;
+    }
+
+}
+
+    }}
+
