49f68c0d131e61df81a4ea9ed80178ed07467e68
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-11-19 12:06:43 +0530

    Add files via upload

---
+++ ArrayList/max1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Max1 {
+
+public List <Integer> findmax(List<Integer>lists){
+
+List <Integer> result1 = new ArrayList<>();
+int max =lists.get(0);
+for(int i=0 ; i<lists.size() ; i++){
+    if(lists.get(i) > max){
+        max = lists.get(i);
+    }
+}
+result1.add(max);
+return result1;
+}
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+
+    List<Integer> l = new ArrayList<>();
+l.add(10);
+l.add(20);
+l.add(1,40);
+l.add(30);
+l.add(50);
+
+Max1  x = new Max1();
+List <Integer> result =    x.findmax(l);
+System.out.print("Maximum number is : " + result);
+
+}
+}
